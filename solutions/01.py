6f7406ce2284875c0355e037df7ac33d6e39914d
ningf02 <112631548+ningf02@users.noreply.github.com>
2025-03-05 12:28:49 +0000

    Update 01.py

--- solutions/01.py
+++ solutions/01.py
@@ -5,7 +5,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_list = {}
         for i, num in enumerate(nums):
-            reminder = target - num
-            if reminder in nums_list:
-                return [i, nums_list[reminder]]
+            remainder = target - num
+            if remainder in nums_list:
+                return [i, nums_list[remainder]]
             nums_list[num] = i
