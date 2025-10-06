2d18677080343297875fd1eb406268aee514efd7
ningf02 <112631548+ningf02@users.noreply.github.com>
2025-10-06 18:16:45 +0100

    Create 2696.py

---
+++ solutions/2696.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for i in s:
+            if i == "B" and stack and stack[-1] == "A":
+                    stack.pop()
+            elif i == "D" and stack and stack[-1] == "C":
+                    stack.pop()
+            else:
+                stack.append(i)
+        return len(stack)
+
+
+
